999e233002cb187f7a92950ede7167d4ff79bfb1
Alejandro Javier Asenjo Nitti <alejandro.asenjo88@gmail.com>
2024-10-19 02:15:14 -0300

    motor.c

--- src/A1A80.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/A1A80/func_800A0E80.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/A1A80/func_800A1020.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/A1A80/func_800A11C0.s")
-
-#pragma GLOBAL_ASM("asm/us/nonmatchings/A1A80/func_800A133C.s")
